337aa67430fde17b4f0ff185478dca30f447a082
Vishal Vaghasiya <vishal@nexios.in>
2025-07-04 16:15:47 +0530

    Add ChildCircelTVCell UITableViewCell and XIB
    Introduced a new UITableViewCell subclass, ChildCircelTVCell, along with its corresponding XIB layout for use in the ChildMode settings. Updated the Xcode project to include these new files in the build sources and resources.

---
+++ LiveLocationTacker/LiveLocationTacker/UI/Settings/ChildMode/XIB/ChildCircelTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChildCircelTVCell.swift
+//  LiveLocationTacker
+//
+//  Created by Nexios Technologies on 04/07/25.
+//
+
+import UIKit
+
+class ChildCircelTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
